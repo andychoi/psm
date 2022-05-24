79efcd896526f068e928d3fbdb26b5624120fed7
Andy Choi <3507699+andychoi@users.noreply.github.com>
2022-05-24 12:20:44 -0700

    template-status to integer

--- psm/models.py
+++ psm/models.py
@@ -58,11 +58,16 @@ class Priority(enum.Enum):
     CRITICAL = '30-critical'
 
 class Status(enum.Enum):
-    NA = '00-notApplicable'
-    GREEN = '10-green'
-    YELLOW = '20-yellow'
-    RED = '20-red'
-    COMPLETED = '90-completed'
+    NA = 0
+    GREEN = 1
+    YELLOW = 2
+    RED = 3
+    COMPLETED = 9
+    # NA = '00-notApplicable'
+    # GREEN = '10-green'
+    # YELLOW = '20-yellow'
+    # RED = '20-red'
+    # COMPLETED = '90-completed'
 
 class PrjType(enum.Enum):
     """
@@ -162,11 +167,16 @@ class Project(models.Model):
     )
 
     STATUS = (
-        (Status.GREEN.value, _('Good')),
-        (Status.YELLOW.value, _('Issue')),
-        (Status.RED.value, _('Roadblock')),
-        (Status.COMPLETED.value, _('Completed')),
-        (Status.NA.value, _('Not started')),
+        (0,"N/A"),
+        (1,"Green"),
+        (2,"Yellow"),
+        (3, "Red"),
+        (9, "Completed")        
+        # (Status.GREEN.value, _('Good')),
+        # (Status.YELLOW.value, _('Issue')),
+        # (Status.RED.value, _('Roadblock')),
+        # (Status.COMPLETED.value, _('Completed')),
+        # (Status.NA.value, _('Not started')),
     )
 
     PRJTYPE = (
@@ -192,10 +202,14 @@ class Project(models.Model):
 
     description = models.TextField(_("description"), max_length=2000, null=True, blank=True)
 
-    status_o = models.CharField(_("status overall"), max_length=20, choices=STATUS, default=Status.GREEN.value)
-    status_t = models.CharField(_("status schedule"), max_length=20, choices=STATUS, default=Status.GREEN.value)
-    status_b = models.CharField(_("status budget"), max_length=20, choices=STATUS, default=Status.GREEN.value)
-    status_s = models.CharField(_("status scope"), max_length=20, choices=STATUS, default=Status.GREEN.value)
+    status_o = models.IntegerField(_("Overall"), choices=STATUS, default=0)
+    status_t = models.IntegerField(_("Schedule"), choices=STATUS, default=0)
+    status_b = models.IntegerField(_("Budget"), choices=STATUS, default=0)
+    status_s = models.IntegerField(_("Scope"), choices=STATUS, default=0)
+    # status_o = models.CharField(_("status overall"), max_length=20, choices=STATUS, default=Status.GREEN.value)
+    # status_t = models.CharField(_("status schedule"), max_length=20, choices=STATUS, default=Status.GREEN.value)
+    # status_b = models.CharField(_("status budget"), max_length=20, choices=STATUS, default=Status.GREEN.value)
+    # status_s = models.CharField(_("status scope"), max_length=20, choices=STATUS, default=Status.GREEN.value)
     resolution = models.TextField(_("PM Memo"), max_length=2000, null=True, blank=True)
     #settings.AUTH_USER_MODEL
     user = models.ForeignKey(ExtendUser, related_name='project_manager', verbose_name=_('HAEA PM'),

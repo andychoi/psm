e1d802b3494cbb543e03cf57d498aa2b58d2ee66
Sungwon Um <sum@autoeveramerica.com>
2022-06-14 09:08:12 -0700

    fix use_ldap bool

--- psmprj/settings.py
+++ psmprj/settings.py
@@ -222,7 +222,8 @@ MEDIA_URL = '/media/'
 # export/import
 #IMPORT_EXPORT_EXPORT_PERMISSION_CODE = 'import '
 
-if (env('USE_LDAP', None) == 'True'):
+USE_LDAP = env.bool('USE_LDAP', False)
+if USE_LDAP:
     # AzureAD SSO
     AUTHENTICATION_BACKENDS = [
     #    'django_auth_adfs.backend.AdfsAuthCodeBackend',

d5e68cf33805135a2e7b42d7334ace0cd3727793
Andy Choi <3507699+andychoi@users.noreply.github.com>
2022-06-21 11:51:01 -0700

    fix urls

--- blog/urls.py
+++ blog/urls.py
@@ -15,10 +15,10 @@ urlpatterns = [
     path('',                        PostListView.as_view(), name='home'),
     path('user/<str:username>/',    UserPostListView.as_view(), name='user_posts'),
     path('post/new/',               PostCreateView.as_view(), name='post_create'),
-    path("post/<slug:slug>/",       PostDetailView.as_view(), name="post_slug_detail"),  # new
     path('post/<int:pk>/update/',   PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete/',   PostDeleteView.as_view(), name='post_delete'),
     path('post/<int:pk>/',          PostDetailView.as_view(), name='post_detail'),
+    path("post/<slug:slug>/",       PostDetailView.as_view(), name="post_slug_detail"),  # new
     path('about/', views.about, name='about'),
     # path('post/<int:pk>/comment/', add_comment, name='add_comment'),
 ]

ed23bf90b4804997299d45a14cb9c7299d953fd4
Andy Choi <3507699+andychoi@users.noreply.github.com>
2022-07-03 18:38:42 -0700

    url fix

--- psmprj/settings.py
+++ psmprj/settings.py
@@ -278,7 +278,7 @@ ADMINS = (
 from .settings_emails import *
 
 # Default login https://docs.djangoproject.com/en/4.0/ref/settings/#login-url
-LOGIN_URL = "/login"
+LOGIN_URL = "login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
